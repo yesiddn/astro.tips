51a49f099a4da5d90ea201b825c915f16bd79f86
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-05-28 17:21:19 -0500

    chore: Add Prettier configuration

---
+++ .prettierrc.mjs
@@ -0,0 +1,16 @@
+/** @type {import("prettier").Config} */
+export default {
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  useTabs: true,
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '**/*astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
