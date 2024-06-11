78fe62dd568bca97528ca17cc39acc2ceab53074
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-06-11 15:54:42 -0500

    Add components in Vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), mdx(), react()], // se agrean todas las integraciones que se deseen, por ejemplo tailwind, react, svelte, etc.
+	integrations: [tailwind(), mdx(), react(), vue()], // se agrean todas las integraciones que se deseen, por ejemplo tailwind, react, svelte, etc.
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,tsx}"],
+	content: ["./src/**/*.{astro,html,js,jsx,tsx,vue}"],
 	theme: {
 		extend: {},
 	},
